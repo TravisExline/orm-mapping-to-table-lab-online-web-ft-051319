7c9b629960ffeacc0f03057cfa5dbbb4c9e5fc43
Travis Exline <t.exline22@gmail.com>
2019-06-17 16:30:40 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,6 @@
 class Student
   attr_accessor :name, :grade
-  attr_reader :id 
+  attr_reader :id
 
   def initialize(name, grade, id = nil)
     @name = name
@@ -8,6 +8,6 @@ class Student
   end
 
   # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]  
-  
+  #  with DB[:conn]
+
 end
